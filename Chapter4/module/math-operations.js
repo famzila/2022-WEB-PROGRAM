885ff0fb3d318b027c0e0059f75e3737f589e341
famzila <famzil.contact@gmail.com>
2022-05-23 16:11:25 +0200

    Module example

---
+++ Chapter4/module/math-operations.js
@@ -0,0 +1,14 @@
+// export the add functionnality
+export const add = (a, b) => {
+    return Number(a) + Number(b);
+}
+
+// export the multiply functionnality
+export const multiply = (a, b) => {
+return Number(a) * Number(b);
+}
+
+// export the devide functionnality
+export const devide = (a, b) => {
+return Number(a) / Number(b);
+}
